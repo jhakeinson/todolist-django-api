19cbea2f39c7b3381ea81cfcc53b2d0b629964d9
Jhake Inson <me@jhakeinson.com>
2023-06-28 09:31:10 +0800

    FIX: get single user not working

--- core/user/viewsets.py
+++ core/user/viewsets.py
@@ -14,13 +14,20 @@ class UserViewSet(viewsets.ModelViewSet):
     ordering = ['-updated']
 
     def get_queryset(self):
+        print('user: ', self.request.user)
         if self.request.user.is_superuser:
+            # return all users
             return User.objects.all()
+        else:
+            # return not permitted to access
+            return User.objects.none()
 
     def get_object(self):
+        print('lookup_field: ', self.lookup_field)
+        print('kwargs: ', self.kwargs)
         lookup_field_value = self.kwargs[self.lookup_field]
 
-        obj = User.objects.get(lookup_field_value)
+        obj = User.objects.get(pk=lookup_field_value)
         self.check_object_permissions(self.request, obj)
 
         return obj
